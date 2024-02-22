f2d3159c509310a7621e8f9f7cfb6868e5626b28
Aishah999 <aishaah@code.edu.az>
2024-02-22 18:26:19 +0400

    first commit

---
+++ method/method/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//#region Task 1
+//int a = 55;
+//int b = 68;
+//int c = 74;
+//int result = 0;
+//if(a>10&&a<100)
+//{
+//result += a;
+//}
+//if (b>10&&b<100)
+//{
+//result += b;
+//}
+//if(c>10&&c<100)
+//{
+//result += c;
+//}
+//Console.WriteLine(result);
+//#endregion 
+
+
+//#region Task 2
+//int month = 9;
+//if (month > 0 && month <= 12)
+//{
+//if (month >= 3 && month <= 5)
+//{
+//Console.WriteLine("yaz");
+
+//}
+//else if (month >= 6 && month <= 8)
+//{
+//Console.WriteLine("yay");
+//}
+//else if (month >= 9 && month <= 11)
+//{
+//Console.WriteLine("payiz");
+//}
+//else
+//{
+//Console.WriteLine("qis");
+//}
+
+//}
+//else
+//{
+//Console.WriteLine("duzgun eded daxil et");
+//}
+
+
+//#endregion
+
+
+//region Task 3
+//int num = 423;
+//if (num > 99 && num < 1000)
+//{
+//int LastNum = num % 10;
+//int secondNum = (num / 10) % 10;
+//int FirstNum = num / 100;
+//Console.WriteLine(FirstNum);
+//Console.WriteLine(secondNum);
+//Console.WriteLine(LastNum);
+
+//}
+//else
+//{
+//Console.WriteLine("duzgun eded daxil et");
+//}
+
+//#endregion
+
+//#region Task 4
+//int a = 40;
+//int b = 50;
+//int c = 20;
+//if(c<a&&c<b)
+//{
+    //Console.WriteLine(a + b);
+//}
+//else if(b<a&&b<c)
+//{
+    //Console.WriteLine(a + c);
+//}
+//else if(a<b&&a<c)
+//{
+    //Console.WriteLine(b + c);
+//}
+
+//#endregion
